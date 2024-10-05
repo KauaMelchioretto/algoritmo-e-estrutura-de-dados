6039bd020f6825653946f6b5a3383957055ce794
Kauã Melchioretto <92891880+KauaMelchioretto@users.noreply.github.com>
2024-10-04 22:04:17 -0300

    implements destruct method and others implementations

--- ListaEncadeada/main.c
+++ ListaEncadeada/main.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Elemento {
+typedef struct Elemento
+{
     struct Elemento *prox;
     int valor;
 } Elemento;
 
-typedef struct Lista {
+typedef struct Lista
+{
     Elemento *inicio;
 } Lista;
 
-Lista * criar_lista() {
+Lista *criar_lista()
+{
     Lista *nova = malloc(sizeof(Lista));
 
-    if(nova == NULL) {
+    if (nova == NULL)
+    {
         return NULL;
     }
 
@@ -21,29 +25,36 @@ Lista * criar_lista() {
     return nova;
 }
 
-Elemento* inserir(Lista* l, int valor) {
+Elemento *inserir(Lista *l, int valor)
+{
     Elemento *elemento, *aux;
 
-    if(l == NULL) {
+    if (l == NULL)
+    {
         return NULL;
     }
 
-     elemento = malloc(sizeof(Elemento));
+    elemento = malloc(sizeof(Elemento));
 
-    if(elemento == NULL) {
+    if (elemento == NULL)
+    {
         return NULL;
     }
 
     elemento->valor = valor;
     elemento->prox = NULL;
 
-    if(l->inicio == NULL) {
+    if (l->inicio == NULL)
+    {
         l->inicio = elemento;
         return elemento;
-    } else {
+    }
+    else
+    {
         aux = l->inicio;
 
-        while(aux->prox != NULL) {
+        while (aux->prox != NULL)
+        {
             aux = aux->prox;
         }
 
@@ -53,75 +64,92 @@ Elemento* inserir(Lista* l, int valor) {
     return elemento;
 }
 
-Elemento* inserir_inicio(Lista* l, int valor) {
+Elemento *inserir_inicio(Lista *l, int valor)
+{
     Elemento *elemento, *aux;
 
-    if (l == NULL) {
+    if (l == NULL)
+    {
         return NULL;
     }
 
     elemento = malloc(sizeof(Elemento));
 
-    if (elemento == NULL) {
+    if (elemento == NULL)
+    {
         return NULL;
     }
-    
+
     elemento->valor = valor;
 
-    if (l->inicio != NULL) {
+    if (l->inicio != NULL)
+    {
         elemento->prox = l->inicio;
         l->inicio = elemento;
         return elemento;
-    } else {
+    }
+    else
+    {
         elemento->prox = NULL;
         l->inicio = elemento;
         return elemento;
     }
 }
 
-Elemento* insere_posicao(Lista* l, int valor, int posicao) {
+Elemento *insere_posicao(Lista *l, int valor, int posicao)
+{
     Elemento *elemento, *auxProx, *auxAtual, *aux;
     int i = 1;
 
-    if (l == NULL) {
+    if (l == NULL)
+    {
         return NULL;
     }
 
     elemento = malloc(sizeof(Elemento));
 
-    if (elemento == NULL) {
+    if (elemento == NULL)
+    {
         return NULL;
     }
 
     elemento->valor = valor;
     aux = l->inicio;
 
-    if(l->inicio == NULL) {
+    if (l->inicio == NULL)
+    {
         elemento->prox = NULL;
         l->inicio = elemento;
-    } else {
+    }
+    else
+    {
         auxAtual = l->inicio;
 
-        while(i < posicao && aux->prox != NULL) {
+        while (i < posicao && aux->prox != NULL)
+        {
             aux = aux->prox;
             i++;
         }
+
         elemento->prox = aux->prox;
         aux->prox = elemento;
     }
 }
 
-Elemento *buscar_por_indice(Lista *l, int posicao){
+Elemento *buscar_por_indice(Lista *l, int posicao)
+{
     Elemento *aux;
     int count = 0;
 
-    if(l == NULL){
+    if (l == NULL)
+    {
         return NULL;
     }
 
     aux = l->inicio;
 
-    while(count != posicao && aux->prox != NULL) {
+    while (count != posicao && aux->prox != NULL)
+    {
         aux = aux->prox;
         count++;
     }
@@ -129,37 +157,45 @@ Elemento *buscar_por_indice(Lista *l, int posicao){
     return aux;
 }
 
-void mostrar_lista(Lista *lista) {
+void mostrar_lista(Lista *lista)
+{
     Elemento *aux;
 
-    if (lista == NULL) {
+    if (lista == NULL)
+    {
+        printf("A lista está vazia!\n");
         return;
     }
 
     aux = lista->inicio;
 
     printf("--- Mostrando Lista ---\n");
-    while(aux != NULL) {
+    while (aux != NULL)
+    {
         printf("Valor: %d\n", aux->valor);
         aux = aux->prox;
     }
 }
 
-void removerDadoPorValor(Lista *lista, int valor) {
+void removerDadoPorValor(Lista *lista, int valor)
+{
     Elemento *auxProximo, *auxAnterior;
 
-    if (lista == NULL) {
+    if (lista == NULL)
+    {
         return;
     }
 
     auxProximo = lista->inicio;
 
-    while(auxProximo->valor != valor) {
+    while (auxProximo->valor != valor)
+    {
         auxAnterior = auxProximo;
         auxProximo = auxProximo->prox;
 
-        if(auxProximo == NULL) {
-            printf("O valor não existe na lista!\n");
+        if (auxProximo == NULL)
+        {
+            printf("O valor '%d' não existe na lista!\n", valor);
             return;
         }
     }
@@ -169,10 +205,31 @@ void removerDadoPorValor(Lista *lista, int valor) {
     free(auxProximo);
 }
 
+Lista* destruir(Lista *lista) {
+    Elemento *auxAnterior, *auxProximo;
+
+    if (lista == NULL) {
+        printf("A lista já foi deletada!\n");
+    }
+
+    auxProximo = lista->inicio;
+
+    while(auxProximo != NULL) { 
+        auxAnterior = auxProximo;
+        auxProximo = auxProximo->prox;
+        free(auxAnterior);
+    }
+
+    free(lista);
+    // Além de dar o free() tem que dar um NULL pois mesmo que dando o free() é acabado puxando outro endereço de memória para esta variável, assim, acaba dando xabu no método de mostrar a lista
+    lista = NULL;
+    printf("Lista deletada!\n");
+    return lista;
+}
 
 int main()
 {
-    Lista* lista = criar_lista();
+    Lista *lista = criar_lista();
     inserir(lista, 1);
     inserir(lista, 2);
     insere_posicao(lista, 5, 1);
@@ -184,6 +241,12 @@ int main()
 
     printf("--- Removendo dado por valor ---\n");
     removerDadoPorValor(lista, 5);
+    inserir_inicio(lista, 99);
+    inserir_inicio(lista, 88);
+    removerDadoPorValor(lista, 10);
     mostrar_lista(lista);
+    lista = destruir(lista);
+    mostrar_lista(lista);
+
     return 0;
 }
