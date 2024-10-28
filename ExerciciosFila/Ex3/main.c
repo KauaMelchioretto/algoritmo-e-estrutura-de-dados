ae948caf4e94052ac0a8314149387c066c078c86
unknown <kauamelchoretto@gmail.com>
2024-10-28 20:44:03 -0300

    finaliza questão 3 de lista de processos

--- ExerciciosFila/Ex3/main.c
+++ ExerciciosFila/Ex3/main.c
@@ -35,14 +35,16 @@ void inserir(Fila* fila) {
     
     process = malloc(sizeof(Process));
     
-    printf("Type process ID\n");
+    printf("Digite o ID do processo\n");
     scanf("%d", &process->id);
-    printf("Type process name\n");
+    printf("Digite o nome do processo\n");
     scanf("%s", process->name);
-    printf("Type process priority\n");
+    printf("Digite a prioridade do processo\n");
     scanf("%d", &process->priority);
-    printf("Type process wait (execution time)\n");
+    printf("Digite o tempo de execução do proceso\n");
     scanf("%d", &process->wait);
+    
+    process->prox = NULL;
     
     if (fila->inicio == NULL) {
         fila->inicio = process;
@@ -52,11 +54,11 @@ void inserir(Fila* fila) {
     }
 
     fila->fim = process;
-    printf("Process saved sucessfully!\n");
+    printf("Processo salvo com sucesso!\n");
 }
 
 void mostrar_fila(Fila* fila) {
-    Aviao *aux;
+    Process *aux;
     int count = 0;
     
     if(fila == NULL) {
@@ -66,13 +68,13 @@ void mostrar_fila(Fila* fila) {
     aux = fila->inicio;
     
     while(aux != NULL) {
-        printf("%d° da lista -> N° do voo %d\n", count++, aux->numero);
+        printf("%d° da lista -> Processo de ID %d, Nome: %s, Prioridade: %d, Tempo execução: %d \n", count++, aux->id, aux->name, aux->priority, aux->wait);
         aux = aux->prox;
     }
 }
 
-int autorizarDecolagem(Fila* fila) {
-    Aviao *aux;
+int executarProcesso(Fila* fila) {
+    Process *aux;
     int retorno = 0;
     
     if(fila == NULL) {
@@ -85,7 +87,7 @@ int autorizarDecolagem(Fila* fila) {
     
     aux = fila->inicio;
     fila->inicio = aux->prox;
-    retorno = aux->numero;
+    retorno = aux->id;
     
     free(aux);
     
@@ -98,8 +100,8 @@ int autorizarDecolagem(Fila* fila) {
     return retorno;
 }
 
-int qtdAvioesNaFila(Fila *fila) {
-    Aviao *aux;
+int qtdProcessos(Fila *fila) {
+    Process *aux;
     int cont = 0;
     
     if(fila == NULL) {
@@ -121,87 +123,44 @@ int qtdAvioesNaFila(Fila *fila) {
     return cont;
 }
 
-void listaPrimeiro(Fila *fila) {
-    Aviao *aux;
-    
-    if(fila == NULL) {
-        return;
+int matarProcessoMaiorTempo(Fila* fila) {
+    if(fila == NULL || fila->inicio == NULL) {
+        return -1;
     }
-    
-    if(fila->inicio == NULL) {
-        printf("Nao ha avioes na fila\n");
-    }
-    
-    aux = fila->inicio;
-    printf("N° do voo: %d, modelo: %s, prefixo: %s, nome da empresa: %s, data: %d/%d/%d %d:%d:%d\n", aux->numero, aux->modelo, aux->prefixo, aux->empresa, aux->dataHora.dia, aux->dataHora.mes, aux->dataHora.ano, aux->dataHora.hora, aux->dataHora.minuto, aux->dataHora.segundo);
-}
 
-void furaFilaPorPosicao(Fila *fila, int posicaoAviao) {
-    Aviao *aux, *auxAnterior;
-    int count = 0;
-    
-    if(fila == NULL) {
-        return;
-    }
-    
-    if(fila->inicio == NULL) {
-        printf("Nao ha avioes na fila\n");
-    }
-    
-    aux = fila->inicio;
-    
-    while(count < posicaoAviao && aux->prox != NULL) {
-        auxAnterior = aux;
+    Process *aux = fila->inicio;
+    Process *processoMaiorTempo = aux;
+    Process *processoAnterior = NULL;
+    Process *anteriorMaiorTempo = NULL;
+
+    while(aux != NULL) {
+        if(aux->wait > processoMaiorTempo->wait) {
+            processoMaiorTempo = aux;
+            anteriorMaiorTempo = processoAnterior;
+        }
+
+        processoAnterior = aux;
         aux = aux->prox;
-        count++;
     }
-    
-    auxAnterior->prox = aux->prox;
-    aux->prox = fila->inicio;
-    fila->inicio = aux;
-    
-    printf("O voo N° %d furou a fila para o inicio\n", aux->numero);
-}
 
-void furaFilaInicio(Fila *fila) {
-    Aviao *aviao, *aux;
-    int numero, modelo;
-    struct DateTime dataHora;
-    
-    if(fila == NULL) {
-        return;
-    }
-    
-    aviao = malloc(sizeof(Aviao));
-    
-    printf("Digite o numero do voo\n");
-    scanf("%d", &numero);
-    aviao->numero = numero;
-    
-    printf("Digite o modelo do aviao\n");
-    scanf("%s", aviao->modelo);
-    
-    printf("Digite o prefixo do aviao\n");
-    scanf("%s", aviao->prefixo);
-    
-    printf("Digite a empresa do aviao\n");
-    scanf("%s", aviao->empresa);
-    
-    dataHora = criaHora();
-    aviao->dataHora = dataHora;
-    aviao->prox = NULL;
-    
-    if (fila->inicio == NULL) {
-        fila->inicio = aviao;
+    if(anteriorMaiorTempo->wait == NULL) {
+        printf("Não há processos com tempo maior do que 0 segundos de execução\n");
+        fila->inicio = processoMaiorTempo->prox;
     } else {
-        aviao->prox = fila->inicio;
-        fila->inicio = aviao;
+        anteriorMaiorTempo->prox = processoMaiorTempo->prox;
     }
 
-    printf("Novo avião furou a fila com sucesso!\n");
-    fila->fim = aviao;
-}
+    if(processoMaiorTempo == fila->fim) {
+        fila->fim = anteriorMaiorTempo;
+    }
 
+
+    int idProcessoMorto = processoMaiorTempo->id;
+    free(processoMaiorTempo);
+    processoMaiorTempo = NULL;
+
+    return idProcessoMorto;
+}
 
 int main() {
     Fila *fila;
@@ -209,23 +168,18 @@ int main() {
     printf("cria fila...\n");
     fila = cria_fila();
     printf("Fila criada...\n");
-    // Exercicio 1
     inserir(fila);
     inserir(fila);
     inserir(fila);
     mostrar_fila(fila);
-    int qtdAvioes = qtdAvioesNaFila(fila);
-    printf("Aviões na fila: %d\n", qtdAvioes);
-    int vooAutorizado = autorizarDecolagem(fila);
-    printf("Voo de N° %d autorizado para decolagem\n", vooAutorizado);
-    listaPrimeiro(fila);
-    // o 1 termina aqui
-    
-    // Exercicio 2
-        // furaFilaPorPosicao(fila, 1);
-    furaFilaInicio(fila);
+    int processoExecutado = executarProcesso(fila);
+    printf("Processo de ID '%d' executado com sucesso!\n", processoExecutado);
     mostrar_fila(fila);
-    // O 2 termina aqui
+    inserir(fila);
+    mostrar_fila(fila);
+    int idProcessoMorto = matarProcessoMaiorTempo(fila);
+    printf("Processo de ID '%d' morto por maior tempo de execução!\n", idProcessoMorto);
+    mostrar_fila(fila);
     
     return 0;
 }
